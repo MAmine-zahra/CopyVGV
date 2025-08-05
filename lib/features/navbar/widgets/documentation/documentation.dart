a29d564b6b7cfe18fded93dd961fa74c93fe63a3
MAmine-zahra <mohamed.amine.zahra@ted-university.com>
2025-08-05 14:34:32 +0100

    Feat: Add data and widgets for example pages
    This commit introduces the following changes:
    
    -   Added JSON data files for "Financial Dashboard", "Airplane Entertainment System", and "Documentation".
    -   Created corresponding image assets for "Financial Dashboard" and "Airplane Entertainment System".
    -   Implemented widget structures for "Financial Dashboard", "Airplane Entertainment System", and "Documentation" pages. These widgets now load and display content dynamically from their respective JSON data files.

--- lib/features/navbar/widgets/documentation/documentation.dart
+++ lib/features/navbar/widgets/documentation/documentation.dart
@@ -1,12 +1,61 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:new_app/Features/navbar/widgets/reusable_widgets/subsection_widget.dart';
+import 'package:new_app/Features/navbar/widgets/reusable_widgets/text_placers.dart';
+import '../../domain/info_json_model.dart';
+
 class DocumentationContent extends StatelessWidget {
   const DocumentationContent({super.key});
+
+  Future<DataJsonModel> loadJsonData() async {
+    final String response = await rootBundle.loadString(
+      'assets/data/documentation_data.json',
+    );
+    final Map<String, dynamic> jsonData = json.decode(response);
+    return DataJsonModel.fromJson(jsonData);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Documentation Page',
-        style: TextStyle(color: Colors.white, fontSize: 24),
+    return SingleChildScrollView(
+      child: FutureBuilder(
+        future: loadJsonData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData) {
+            return const Center(child: Text('No data found.'));
+          }
+
+          final shownData = snapshot.data!;
+          return ContentPage(
+            mainTitle: shownData.mainTitle ?? '_',
+            contentChildren: List.generate((shownData.content ?? []).length, (
+                index,
+                ) {
+              final item = (shownData.content ?? [])[index];
+              switch (item.type) {
+                case "subtitle":
+                  return SubsectionWidget(subtitle: item.text ?? '_');
+                case "text":
+                  return SubsectionWidget(textItems: [item.text ?? '_']);
+                case "bulletPoints":
+                  return SubsectionWidget(bulletPoints: item.points ?? []);
+                case "code2":
+                  return SubsectionWidget(goodCode: item.goodCode ?? '', badCode: item.badCode ?? '',);
+                case "tip":
+                  return SubsectionWidget(tip: item.tip ?? '_', tipCode: item.code ?? '');
+                case "spacer":
+                  return const SizedBox(height: 16);
+                default:
+                  return const SizedBox.shrink();
+              }
+            }),
+          );
+        },
       ),
     );
   }
