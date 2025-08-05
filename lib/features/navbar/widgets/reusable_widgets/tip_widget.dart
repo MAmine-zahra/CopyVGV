c251dd5ebef513a418f217aae9f7b34162754742
MAmine-zahra <mohamed.amine.zahra@ted-university.com>
2025-08-05 17:21:53 +0100

    Fix: Correct typo "Vehicule" to "Vehicle"
    This commit corrects a typo in the "Vehicle Cockpit" navigation item and related files.
    
    Additionally, the "Vehicle Cockpit" page now loads its content dynamically from a JSON file (`assets/data/vehicle_cockpit_data.json`).
    This includes text, subtitles, code snippets, images, and other content types.
    The page now displays the content in a structured format with appropriate widgets for each content type.
    The following new images have been added:
    - `assets/images/speedometer.png`
    - `assets/images/vehicle_cockpit.png`

--- lib/features/navbar/widgets/reusable_widgets/tip_widget.dart
+++ lib/features/navbar/widgets/reusable_widgets/tip_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_app/Features/navbar/widgets/reusable_widgets/subsection_widget.dart';
 class TipWidget extends StatelessWidget {
   final String tip;
   final String code;
@@ -37,6 +38,7 @@ class TipWidget extends StatelessWidget {
               ),
             ),
             SizedBox(height: 8),
+            code == '' ? const SizedBox.shrink() : SubsectionWidget(code: code ,)
           ],
         ),
       ),
